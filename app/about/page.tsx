05643dd9b4df4d0ef3a470adc867e88583d11342
minhbodoivl <anhminh23072004@gmail.com>
2025-12-29 16:49:59 +0700

    learning about React.js

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <main className="p-10">
+      <h1 className="text-2xl font-bold">About Me</h1>
+      <p className="mt-4">
+        My name is Nguyễn Đình Anh Minh.
+      </p>
+      <p className="mt-4">
+        I was born on July 23, 2004. I studied at Hutech university.
+      </p>
+      <p className="mt-4">
+        I am a programmer specializing in C#, CSS, and HTML.
+      </p>
+    </main>
+  );
+}
