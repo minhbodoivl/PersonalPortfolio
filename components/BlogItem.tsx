11a1797d0484762ec77947a02101f5a227b37027
minhbodoivl <anhminh23072004@gmail.com>
2026-01-04 18:35:07 +0700

    Add lib, content, data

---
+++ components/BlogItem.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import { posts } from "@/data/posts"
+
+type BlogItemProps = {
+    title: string;
+    slug: string;
+}
+
+export default function BlogItem({title, slug}: BlogItemProps)
+{
+    const post = posts.find(p => p.slug === slug)
+
+    return (
+        <Link href={`/blog/${post?.slug}`}>
+            <section className="border p-4 mt-4 hover:bg-gray-50">
+                <h2 className="text-xl font-semibold"> {title} </h2>
+            </section>
+        </Link>
+    )
+}
